baefd459d39b74bad63c5ae4c3cef4461742ebaf
Gareth Rushgrove <gareth@morethanseven.net>
2010-03-13 18:18:16 +0000

    moving some code around, oh and making the xml index page work

--- dumper.py
+++ dumper.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
 
-import os
 import sys
 import ConfigParser
 
 import MySQLdb
 import MySQLdb.cursors
-import simplejson
 
-config = ConfigParser.RawConfigParser()
+
+from lib import ensure_dir, output_json, output_xml
+
 try:
+    config = ConfigParser.RawConfigParser()
     config.read(sys.argv[1])
 except IndexError:
     print "You must pass a configuration file as the first agument"
@@ -29,40 +30,6 @@ except ConfigParser.NoSectionError, e:
     print e
     sys.exit(2)
 
-def create_or_update(content, filename):
-    """
-    Given some content and a filename either:
-    - create the file and write the content to it if it doesn't exist
-    - update the file contents if they differ
-    - do nothing if the file hasn't changed
-    """
-    # work out whether we are creating, updating or doing nothing
-    try:
-        handle = open(filename, 'r')
-        created = False 
-        if not content == handle.read():
-            updated = True
-        else:
-            updated = False
-    except IOError:
-        created = True
-        updated = False
-    
-    # write the output to the file and close if we have to
-    if created or updated:
-        handle = open(filename, 'w')
-        handle.write(content)
-        handle.close()
-
-    return created, updated
-
-def ensure_dir(path):
-    "Create directory if it doesn't already exist"
-    directory = os.path.dirname("%s/" % path)
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-        return True
-    return False
 
 if __name__ == '__main__':
 
@@ -86,15 +53,11 @@ if __name__ == '__main__':
     index = []
     for result in results:
         # filename based on the specified INDEX
-        file_name = "%s/%s.json" % (PATH, result[INDEX])
-        # convert the data structure to json
-        output = simplejson.dumps(result)
-        created, updated = create_or_update(output, file_name)
-        # print output
-        if created:
-            print "[Created] %s" % file_name
-        if updated:
-            print "[Updated] %s" % file_name
+        
+        record = "%s/%s" % (PATH, result[INDEX])
+        output_json(result, record)
+        output_xml(result, record)
+        
         # we also want an index page so as we loop through we
         # create a list of all the individual items based
         # on the specified index value
@@ -103,11 +66,8 @@ if __name__ == '__main__':
             "url":  "/%s/%s" % (PATH, result[INDEX])
         })
     # create the index page
-    index_file_name = "%s.json" % PATH
-    index_json = simplejson.dumps(index)
-    created, updated = create_or_update(index_json, index_file_name)
-    if created:
-        print "[Created] %s" % index_file_name
-    if updated:
-        print "[Updated] %s" % index_file_name
+    output_json(index, PATH)
+    output_xml(index, PATH)
+    
+
 
---
+++ lib.py
@@ -0,0 +1,101 @@
+import os
+from xml.etree import ElementTree
+from xml.etree.ElementTree import tostring
+
+import simplejson
+
+
+def _ConvertDictToXmlRecurse(parent, dictitem):
+    assert type(dictitem) is not type([])
+
+    if isinstance(dictitem, dict):
+        for (tag, child) in dictitem.iteritems():
+            if str(tag) == '_text':
+                parent.text = str(child)
+            elif type(child) is type([]):
+                # iterate through the array and convert
+                for listchild in child:
+                    elem = ElementTree.Element(tag)
+                    parent.append(elem)
+                    _ConvertDictToXmlRecurse(elem, listchild)
+            else:
+                elem = ElementTree.Element(tag)
+                parent.append(elem)
+                _ConvertDictToXmlRecurse(elem, child)
+    else:
+        parent.text = str(dictitem)
+    
+def dict_to_xml(xmldict, element):
+    """
+    Converts a dictionary to an XML ElementTree Element 
+    """
+
+    root = ElementTree.Element(element)
+    _ConvertDictToXmlRecurse(root, xmldict)
+    return tostring(root)
+
+def create_or_update(content, filename):
+    """
+    Given some content and a filename either:
+    - create the file and write the content to it if it doesn't exist
+    - update the file contents if they differ
+    - do nothing if the file hasn't changed
+    """
+    # work out whether we are creating, updating or doing nothing
+    try:
+        handle = open(filename, 'r')
+        created = False 
+        if not content == handle.read():
+            updated = True
+        else:
+            updated = False
+    except IOError:
+        created = True
+        updated = False
+
+    # write the output to the file and close if we have to
+    if created or updated:
+        handle = open(filename, 'w')
+        handle.write(content)
+        handle.close()
+
+    return created, updated
+
+def ensure_dir(path):
+    "Create directory if it doesn't already exist"
+    directory = os.path.dirname("%s/" % path)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+        return True
+    return False
+
+def output_json(result, record):
+    file_name = "%s.json" % record
+    # convert the data structure to json
+    output = simplejson.dumps(result)
+    created, updated = create_or_update(output, file_name)
+    # print output
+    if created:
+        print "[Created] %s" % file_name
+    if updated:
+        print "[Updated] %s" % file_name
+
+def output_xml(struct, record):
+    from lib import dict_to_xml
+    file_name = "%s.xml" % record
+    # convert the data structure to json
+
+    if type(struct) == dict:
+        output = dict_to_xml(struct, 'element')
+    if type(struct) == list:
+        output = ""
+        for item in struct:
+            output = output + dict_to_xml(item, 'element')
+        output = "<elements>%s</elements>" % output
+
+    created, updated = create_or_update(output, file_name)
+    # print output
+    if created:
+        print "[Created] %s" % file_name
+    if updated:
+        print "[Updated] %s" % file_name
