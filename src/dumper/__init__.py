dfdebd406ecbd0ab6c0003785ea518a1fc20d1db
Gareth Rushgrove <gareth@morethanseven.net>
2010-03-14 13:39:44 +0000

    backend if now specified in the config file

--- src/dumper/__init__.py
+++ src/dumper/__init__.py
@@ -1,10 +1,12 @@
 import sys
+import ConfigParser
 
 import MySQLdb
 import MySQLdb.cursors
 import simplejson
 
 from lib import ensure_dir, dict_to_xml, create_or_update
+
 
 def Dumper(type):
     "Factory function which returns a matching class"
@@ -75,9 +77,9 @@ class MySQLDumper(BaseDumper):
             DATABASE = config.get('Database', 'database')
 
             # and settings used by this dumper
-            self.sql = config.get('Options', 'sql')
-            self.path = config.get('Options', 'path')
-            self.index = config.get('Options', 'index')
+            self.sql = config.get('Database', 'sql')
+            self.path = config.get('Dumper', 'path')
+            self.index = config.get('Dumper', 'index')
         except (ConfigParser.NoSectionError, ConfigParser.NoOptionError), e:
             print e
             sys.exit(2)
@@ -98,20 +100,23 @@ class MySQLDumper(BaseDumper):
         # get a cursor to make queries with
         self.cursor = MySQLdb.cursors.DictCursor(database)
         
-    def dump(self):
+    def dump(self, output_location):
         "Generate output"
         self.cursor.execute(self.sql)
         results = self.cursor.fetchall()
+        
+        full_path = "%s/%s" % (output_location, self.path)
+        
         # see if the folder PATH exists and if not create it
-        if ensure_dir(self.path):
-            print "\033[1;33m[Created]\033[1;m %s" % self.path
+        if ensure_dir(full_path):
+            print "\033[1;33m[Created]\033[1;m %s" % full_path
 
         # placeholder for list for index page
         index = []
         for result in results:
             # filename based on the specified INDEX
 
-            record = "%s/%s" % (self.path, result[self.index])
+            record = "%s/%s" % (full_path, result[self.index])
             self.output_json(result, record)
             self.output_xml(result, record)
 
@@ -123,5 +128,5 @@ class MySQLDumper(BaseDumper):
                 "url":  "/%s/%s" % (self.path, result[self.index])
             })
         # create the index page
-        self.output_json(index, self.path)
-        self.output_xml(index, self.path)
+        self.output_json(index, full_path)
+        self.output_xml(index, full_path)
--- src/dumper/lib/commands.py
+++ src/dumper/lib/commands.py
@@ -1,17 +1,91 @@
+import os
 import sys
 import ConfigParser
+import getopt
 
 from dumper import Dumper
 
-def main(args):
+
+def main(argv):
     "Run the dumper command app"
+    
+    # parse out arguments
     try:
-        config = ConfigParser.RawConfigParser()
-        config.read(args[1])
+        opts, args = getopt.getopt(argv, "ho:cu",
+           ["help", "output=", "clean"])
+    except getopt.GetoptError:
+        # any errors exit
+        usage()
+        sys.exit(2)
+    
+    # default output to a directory called output
+    output = 'output'
+    
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            # asked for help information
+            usage()
+            sys.exit()
+        elif opt in ("-o", "--output"): 
+            # set a different output dir
+            output = arg
+        elif opt in ("-c", "--clean"):
+            # delete everything from the output dir and exit
+            clean(output)
+            sys.exit()
+    
+    # check we have a configuration file passed in, and that the 
+    # file exists
+    try:
+        if not os.path.isfile(argv[-1]):
+            print "You must specify an existing configuration file"
+            sys.exit(2)
+        
     except IndexError:
         print "You must pass a configuration file as the first agument"
         sys.exit(2)
 
-    DumperClass = Dumper('mysql')
+    # parse the configuration file
+    config = ConfigParser.RawConfigParser()
+    config.read(argv[-1])
+
+    # get the specified backend from the configuration file
+    try:
+        backend = config.get('Dumper', 'backend')
+    except (ConfigParser.NoSectionError, ConfigParser.NoOptionError), e:
+        # if we don't find anything then exit with an error
+        print e
+        sys.exit(2)
+
+    # work out which class to use via the factory
+    try:
+        DumperClass = Dumper(backend)
+    except ValueError:
+        print "Specified backend %s doesn't exist" % backend
+        sys.exit(2)
+    # instantiate the relevant backend class
     dumper = DumperClass(config)
-    dumper.dump()
+    # and finally create all the files
+    dumper.dump(output)
+    
+def usage():
+    "Print help information"
+    print """Dumper. Static generator for web services. Usage: 
+    
+dumper [options] configuration_file
+
+-o, --output [location]    defaults to output
+-c, --clean                removes all files from the output directory
+-h, --help                 display this help message
+"""
+
+def clean(directory):
+    "Remove everything from the output directory"
+    file_list = os.listdir(directory)
+    for individual_file in file_list: 
+        file_path = "%s/%s" % (directory, individual_file)
+        if os.path.isfile(file_path):
+            os.unlink(file_path)
+            print "\033[1;31m[Deleted]\033[1;m %s" % file_path
+        if os.path.isdir(file_path):
+            print "doesn't delete directories yet %s" % file_path
