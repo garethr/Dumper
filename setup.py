eb8e2b259f304a49a326a749f5e8da1b2d8de0ff
Gareth Rushgrove <gareth@morethanseven.net>
2010-03-14 20:57:56 +0000

    updated readme file with new setup.py ways

--- setup.py
+++ setup.py
@@ -3,8 +3,21 @@ from setuptools import setup, find_packages
 setup(
     name = "dumper",
     version = "0.1",
-    
+    author = "Gareth Rushgrove",
+    author_email = "gareth@morethanseven.net",
+    url = "http://github.com/garethr/dumper",
+    install_requires = [
+        "simplejson",
+        "MySQL-python",
+    ],
+    license = "MIT License",
     packages = find_packages('src'),
     package_dir = {'':'src'},
     scripts = ['src/dumper/bin/dumper'],
+    classifiers = [
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ]
 )
